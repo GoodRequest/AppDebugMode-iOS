e352aa7ecb09d60dbd7e7fd7cf99b6663f7dea2a
Lukas Kubaliak <lukas.kubaliak@goodrequest.com>
2024-07-17 14:27:04 +0200

    apns request

--- AppDebugMode-iOS-Sample/AppDebugMode-iOS-Sample/Screens/Home/HomeViewController.swift
+++ AppDebugMode-iOS-Sample/AppDebugMode-iOS-Sample/Screens/Home/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Security
 
 final class HomeViewController: BaseViewController<HomeViewModel> {
     
@@ -51,6 +52,13 @@ final class HomeViewController: BaseViewController<HomeViewModel> {
         return button
     }()
     
+    private let pushNotificationButton: BasicButton = {
+        let button = BasicButton()
+        button.setTitle("Send Bistro PUSH notification", for: .normal)
+        
+        return button
+    }()
+    
 }
 
 // MARK: - Lifecycle
@@ -83,6 +91,10 @@ extension HomeViewController {
         userDefaultsModeButton.publisher(for: .touchUpInside)
             .sink { viewModel.goToSettings()}
             .store(in: &cancellables)
+        
+        pushNotificationButton.publisher(for: .touchUpInside)
+            .sink { viewModel.send(from: self) }
+            .store(in: &cancellables)
     }
     
 }
@@ -102,7 +114,7 @@ private extension HomeViewController {
     
     func addSubviews() {
         [descriptionLabel, stackView].forEach { view.addSubview($0) }
-        [loginModeButton, fetchModeButton, userDefaultsModeButton].forEach { stackView.addArrangedSubview($0) }
+        [loginModeButton, fetchModeButton, userDefaultsModeButton, pushNotificationButton].forEach { stackView.addArrangedSubview($0) }
     }
     
     func setupConstraints() {
@@ -115,6 +127,45 @@ private extension HomeViewController {
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.Insets.edgeInset),
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.Insets.edgeInset),
         ])
+    }
+    
+}
+
+extension HomeViewController: URLSessionDelegate {
+    
+    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        guard let certificateURL = Bundle.main.url(forResource: "cert", withExtension: "cer"),
+              let certificateData = try? Data(contentsOf: certificateURL)
+        else {
+            print("error loading cert file")
+            return
+        }
+        guard let keyURL = Bundle.main.url(forResource: "key", withExtension: "pem"),
+              let keyData = try? Data(contentsOf: keyURL)
+        else {
+            print("error loading key file")
+            return
+        }
+        
+        if let certificate = SecCertificateCreateWithData(nil, certificateData as CFData) {
+            var options: [CFString: Any] = [:]
+            options[kSecImportExportPassphrase] = "1234"
+            
+            var items: CFArray?
+            let securityError = SecPKCS12Import(keyData as CFData, options as CFDictionary, &items)
+            
+            guard securityError == errSecSuccess,
+                  let array = items as? [[String: Any]],
+                  let identity = array.first?[kSecImportItemIdentity as String] else {
+                print("Error creating SecIdentity")
+                completionHandler(.cancelAuthenticationChallenge, nil)
+                return
+               }
+               
+            let credential = URLCredential(identity: identity as! SecIdentity, certificates: [certificate], persistence: .forSession)
+            completionHandler(.useCredential, credential)
+        }
+        
     }
     
 }
--- AppDebugMode-iOS-Sample/AppDebugMode-iOS-Sample/Screens/Home/HomeViewModel.swift
+++ AppDebugMode-iOS-Sample/AppDebugMode-iOS-Sample/Screens/Home/HomeViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 final class HomeViewModel {
 
@@ -35,6 +36,47 @@ extension HomeViewModel {
     
     func goToSettings() {
         coordinator.navigate(to: .home(.goToSettings))
+    }
+    
+    func send(from controller: UIViewController) {
+        let topic = "sk.azet.Bistro-sk"
+        let pushType = "alert"
+        let deviceToken = "80b03e35c3f192f423bd0686f0aa7bde37868b314ae09c2a553cd8c9c96f78898b703bcaa01e28e237bf68dc89d978e15afd7fd876668cdfe0d1409066bc69d5d27708dcf8f5efa499180643b75ce45f"
+        let payload = [
+            "aps": [
+                "alert": [
+                    "title": "Title",
+                    "body": "Body"
+                ]
+            ]
+        ]
+        
+        if let url = URL(string: "https://api.sandbox.push.apple.com/3/device/\(deviceToken)"),
+           let jsonData = try? JSONSerialization.data(withJSONObject: payload, options: []) {
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.setValue(topic, forHTTPHeaderField: "apns-topic")
+            request.setValue(pushType, forHTTPHeaderField: "apns-push-type")
+            request.httpBody = jsonData
+            
+            let session = URLSession(configuration: .default, delegate: controller as? URLSessionDelegate, delegateQueue: nil)
+            
+            let task = session.dataTask(with: request) { data, response, error in
+                if let error {
+                    print("error: \(error)")
+                    return
+                }
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    print("invalid response")
+                    return
+                }
+                
+                print("Response: \(httpResponse.statusCode)")
+            }
+            task.resume()
+        }
+        
     }
     
 }
